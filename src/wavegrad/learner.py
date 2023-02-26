fc932668b3cd0a8473bd55d84dbd7fb893944429
danielezertau <danielezer@mail.tau.ac.il>
2023-02-26 21:52:25 +0200

    Add 1e-4 to the noise scale

--- src/wavegrad/learner.py
+++ src/wavegrad/learner.py
@@ -139,7 +139,7 @@ class WaveGradLearner:
             noise_coef = (1.0 - noise_scale ** 2) ** 0.5
             noisy_audio = noise_scale * audio + noise_coef * noise
             predicted_noise = self.model(noisy_audio, spectrogram, noise_scale.squeeze(1))
-            noise_scale[noise_scale <= 1e-4] = 1e-4
+            noise_scale += 1e-4
             predicted_audio = (noisy_audio - noise_coef * predicted_noise) / noise_scale
             loss = self.loss_fn(audio, predicted_audio.squeeze(1))
 
