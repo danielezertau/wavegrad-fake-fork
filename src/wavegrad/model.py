6000ba1b9f0d5815fb41919b4b79d9be0bf51646
danielezertau <danielezer@mail.tau.ac.il>
2023-02-28 22:57:15 +0200

    Add debug printing

--- src/wavegrad/model.py
+++ src/wavegrad/model.py
@@ -179,9 +179,11 @@ class WaveGrad(nn.Module):
   def forward(self, audio, spectrogram, noise_scale):
     x = audio.unsqueeze(1)
     downsampled = []
-    for film, layer in zip(self.film, self.downsample):
+    for i, (film, layer) in enumerate(zip(self.film, self.downsample)):
       x = layer(x)
+      print(f"Layer {i} output: {x}")
       downsampled.append(film(x, noise_scale))
+      print(f"Film output: {downsampled[-1]}")
 
     x = self.first_conv(spectrogram)
     for layer, (film_shift, film_scale) in zip(self.upsample, reversed(downsampled)):
