7f164b0a9805040b96e73e6c2ae5bdfd6fa4f2af
danielezertau <danielezer@mail.tau.ac.il>
2023-03-02 18:27:49 +0200

    Add small epsilon before calculating the loss sqrt

--- src/wavegrad/learner.py
+++ src/wavegrad/learner.py
@@ -196,7 +196,7 @@ class WaveGradLearner:
             if torch.isnan(S_x).any() or torch.isnan(S_G_x).any():
                 print("Found NAN in spectrogram!")
             loss = loss_weight * ((S_x - S_G_x).abs().sum() + alpha_s * (
-                    ((torch.log(S_x.abs() + eps) - torch.log(S_G_x.abs() + eps)) ** 2).sum(dim=-2) ** 0.5).sum())
+                    (((torch.log(S_x.abs() + eps) - torch.log(S_G_x.abs() + eps)) ** 2).sum(dim=-2) + eps) ** 0.5).sum())
             print(f'Loss for window length {s} is {loss}')
             L += loss
         return L
